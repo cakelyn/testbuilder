ce64bdce18c228e873904ded41b0e18162f1aed1
Katelyn Stamas <cakelyn@gmail.com>
2017-09-10 14:45:54 -0400

    Added Diner's Club and American Express credit card network detection

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -13,6 +13,17 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  if (cardNumber.length === 14) {
+    if (cardNumber[0] === '3' && cardNumber[1] === '8' || cardNumber[1] === '9') {
+      return "Diner's Club";
+    }
+  } else if (cardNumber.length === 15) {
+    if (cardNumber[0] === '3' && cardNumber[1] === '4' || cardNumber[1] === '7') {
+      return "American Express";
+    }
+  }
+
+  return "No network detected";
+
 };
-
-

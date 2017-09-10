2dde7d9056dc10b065388222222441d525b4a300
Katelyn Stamas <cakelyn@gmail.com>
2017-09-10 15:07:04 -0400

    Added Visa and MasterCard functionality and fixed formatting

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,21 +8,45 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-
+  // Detects the Diner's Club network
+  // always starts with a 38 or 39 and is 14 digits long
   if (cardNumber.length === 14) {
-    if (cardNumber[0] === '3' && cardNumber[1] === '8' || cardNumber[1] === '9') {
+    if (cardNumber[0] === '3' &&
+        cardNumber[1] === '8' ||
+        cardNumber[1] === '9') {
       return "Diner's Club";
     }
+
+  // Detects American Express
+  // always starts with 34 or 37 and is 15 digits long
   } else if (cardNumber.length === 15) {
-    if (cardNumber[0] === '3' && cardNumber[1] === '4' || cardNumber[1] === '7') {
+    if (cardNumber[0] === '3' &&
+        cardNumber[1] === '4' ||
+        cardNumber[1] === '7') {
       return "American Express";
     }
-  }
+
+  // Detects Visa
+  // always has a prefix of 4 and a length of 13, 16, or 19
+  } else if ((cardNumber.length === 13 ||
+              cardNumber.length === 16 ||
+              cardNumber.length === 19) &&
+              cardNumber[0] === '4') {
+    return "Visa";
+
+  // Detects MasterCard
+  // always has a prefix of 51, 52, 53, 54, or 55 and a length of 16
+} else if (cardNumber.length === 16 &&
+           cardNumber[0] === '5' &&
+           (cardNumber[1] === '1' ||
+            cardNumber[1] === '2' ||
+            cardNumber[1] === '3' ||
+            cardNumber[1] === '4' ||
+            cardNumber[1] === '5')) {
+  return "MasterCard";
+}
+
 
   return "No network detected";
 
